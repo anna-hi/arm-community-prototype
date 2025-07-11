cbc8ccd146fafff41f8be6ac4612714720997991
Anna Ji <113471421+anna-hi@users.noreply.github.com>
2025-07-11 08:31:08 -0400

    news event card + some css to fix leading the v0 made (bad v0)

--- components/ui/card.tsx
+++ components/ui/card.tsx
@@ -22,7 +22,7 @@ CardHeader.displayName = "CardHeader"
 const CardTitle = React.forwardRef<HTMLDivElement, React.HTMLAttributes<HTMLDivElement>>(
   ({ className, ...props }, ref) => (
     <h2>
-      <div ref={ref} className={cn("leading-none tracking-tight", className)} {...props} />
+      <div ref={ref} className={cn("", className)} {...props} />
     </h2>
   ),
 )
